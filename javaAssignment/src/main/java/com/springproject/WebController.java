80c6dafbf2e2a2f512fcda443d1fe49188cd68fb
renga01 <rengasings@gmail.com>
2023-09-07 18:17:55 +0530

    Did basic error handling.

--- javaAssignment/src/main/java/com/springproject/WebController.java
+++ javaAssignment/src/main/java/com/springproject/WebController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -172,7 +173,10 @@ public class WebController {
 		  return redirectView;
 		 
 	}
-	
+	@PostMapping("/error")
+	public String error() {
+		return "Error";
+	}
 	/* Working Code
 	 * 
 	 * 	@PostMapping(value = "/login")
